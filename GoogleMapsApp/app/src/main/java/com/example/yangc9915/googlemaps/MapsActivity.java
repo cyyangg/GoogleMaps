040722ec3f4efacee25f35058c1df73bd3db81fd
cyyangg <cly9915@gmail.com>
2017-05-25 09:25:15 -0700

    Got it to work finally; Adjusted marker location, adjusted interface
    On start, marker lands on birthplace. when clicked, displays message
    "born here"
    Working on location tracker

--- GoogleMapsApp/app/src/main/java/com/example/yangc9915/googlemaps/MapsActivity.java
+++ GoogleMapsApp/app/src/main/java/com/example/yangc9915/googlemaps/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.yangc9915.googlemaps;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -9,6 +12,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -23,6 +27,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        /*
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        */
     }
 
 
@@ -39,6 +59,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
         /*
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
@@ -47,9 +68,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         */
 
         //Add a marker in birth place and move the camera
-        LatLng boise = new LatLng(43, 116);
-        mMap.addMarker(new MarkerOptions().position(boise).title("Born Here"));
+        LatLng boise = new LatLng(43, -116);
+        mMap.addMarker(new MarkerOptions().position(boise).title("born here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(boise));
 
+
     }
+
+
+
+
 }
