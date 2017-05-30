b614e2f83cced8c02a8423134951dd2aa0c229c2
cyyangg <cly9915@gmail.com>
2017-05-30 09:29:06 -0700

    Added in tracking method, tried to figure out toggleView, set up some menu thing but it isn't found on app(???)
    view changer doesn't work because somehow it's not found
    what's the LocationListener supposed to do?

--- GoogleMapsApp/app/src/main/java/com/example/yangc9915/googlemaps/MapsActivity.java
+++ GoogleMapsApp/app/src/main/java/com/example/yangc9915/googlemaps/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.yangc9915.googlemaps;
 
 import android.*;
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
@@ -9,6 +10,7 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -24,6 +26,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private LocationManager locationManager;
+    private boolean isGPSenabled = false;
+    private boolean isNetworkEnabled = false;
+    private boolean canGetLocation=false;
+    private static final long MIN_TIME_BW_UPDATES = 1000*15*1;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 5;
+
+    private static final CharSequence[] MAP_TYPE_ITEMS = {"Road Map", "Hybrid", "Satellite", "Terrain"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +69,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng boise = new LatLng(43, -116);
         mMap.addMarker(new MarkerOptions().position(boise).title("born here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(boise));
-/*
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+        //current location
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.d("GoogleMaps", "Failed Permission check 1");
             Log.d("GoogleMaps", Integer.toString(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)));
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
@@ -67,34 +81,118 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
         }
 
+
         mMap.setMyLocationEnabled(true);
-
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, true);
-        Location myLocation = locationManager.getLastKnownLocation(provider);
-
-        double latitude = myLocation.getLatitude();
-        double longitude = myLocation.getLongitude();
-
-        LatLng current = new LatLng(latitude, longitude);
-
-        mMap.addMarker(new MarkerOptions().position(current).title("current location"));
-
-
     }
 
-    public void toggleView(GoogleMap googleMap){
-        int mapType = mMap.getMapType();
+    public void getLocation(){
+        try{
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        if (mapType == googleMap.MAP_TYPE_NORMAL){
-            googleMap.setMapType(googleMap.MAP_TYPE_HYBRID);
+            //get GPS status
+            isGPSenabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if(isGPSenabled) Log.d("MyMaps", "getLocation: GPS enabled");
+
+            //get network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if(isNetworkEnabled) Log.d("MyMaps", "getLocation: Network enabled");
+
+            if(!isGPSenabled && !isNetworkEnabled){
+                Log.d("MyMaps", "getLocation: No provider is enabled");
+
+
+            } else {
+                this.canGetLocation = true;
+
+                if(isNetworkEnabled){
+                    Log.d("MyMaps", "getLocation: Network enabled - requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerNetwork);
+
+                    Log.d("MyMaps", "getLocation: NetworkLocation update request successful");
+                    Toast.makeText(this, "Using Network", Toast.LENGTH_SHORT);
+                }
+
+                if(isGPSenabled){
+                    Log.d("MyMaps", "getLocation: GPS enabled - requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerGPS);
+
+                    Log.d("MyMaps", "getLocation: GPSLocation update request successful");
+                    Toast.makeText(this, "Using Network", Toast.LENGTH_SHORT);
+                }
+            }
+
+        }catch (Exception e){
+            Log.d("MyMaps", "Caught exception in getLocation");
+            e.printStackTrace();
         }
-        else if(mapType == googleMap.MAP_TYPE_HYBRID){
-            googleMap.setMapType(googleMap.MAP_TYPE_NORMAL);
-        }
     }
 
-*/
+    public void onLocationChanged(Location location){
+
+        Log.d("MyMaps", "onLocationChanged");
+
+        //when location changes, zoom in on location
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude()));
+        this.mMap.moveCamera(center);
+
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+        this.mMap.animateCamera(zoom);
+
     }
+
+    
+
+    public void toggleView(GoogleMap googleMaps){
+        //prepare dialog by setting up builder
+        final String fDialogTitle = "Select Map Type";
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(fDialogTitle);
+        Log.d("MyMaps", "toggleView: dialog builder success");
+
+        //find current map type
+        int checkItem = mMap.getMapType()-1;
+        Log.d("MyMaps", "toggleView: found map type");
+
+        //add onClickListener so selection will be handled
+        builder.setSingleChoiceItems(MAP_TYPE_ITEMS, checkItem, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item){
+                Log.d("MyMaps", "toggleView: ???");
+                //Perform action depending on which item was selected
+                switch(item)
+                {
+                    case 1:
+                        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                        break;
+                    case 2:
+                        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                        break;
+                    case 3:
+                        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                        break;
+                    default:
+                        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+                dialog.dismiss();
+            }
+
+        }
+        );
+
+        //build dialog and show
+        AlertDialog fMapTypeDialog = builder.create();
+        fMapTypeDialog.setCanceledOnTouchOutside(true);
+        fMapTypeDialog.show();
+
+    }
+
+
+
+
+
 }
